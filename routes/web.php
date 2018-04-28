684b14a84e64557b9a6baa549268a457440450a1
francisco-tequida <francisco.tequida00@gmail.com>
2018-04-28 12:34:47 -0700

    primer commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'RoutesController@index');
+
+Route::get('/infraestructura', 'RoutesController@infraestructura');
+
+Route::get('/equipamiento', 'RoutesController@equipamiento');
